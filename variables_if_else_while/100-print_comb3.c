2dd6dccafcd8a38acc5393847fbfdb731c4a170f
Lujain Alsultan <lujainabdulmohsen@hotmail.com>
2026-07-06 16:12:03 +0300

    Add 100-print_comb3

---
+++ variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a;
+	int b;
+
+	for (a = 0; a < 9; a++)
+	{
+		for (b = a + 1; b < 10; b++)
+		{
+			putchar(a + '0');
+			putchar(b + '0');
+
+			if (a != 8 || b != 9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
