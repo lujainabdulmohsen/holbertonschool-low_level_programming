b6ed3d8f9c080187ae023794c9be9dcf8dfd67a8
Lujain Alsultan <lujainabdulmohsen@hotmail.com>
2026-07-09 18:33:16 +0300

    Add simple calculator

---
+++ simple-calculator/calculator.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int choice = -1;
+
+	while (choice != 0)
+	{
+		printf("Simple Calculator\n");
+		printf("1) Add\n");
+		printf("2) Subtract\n");
+		printf("3) Multiply\n");
+		printf("4) Divide\n");
+		printf("0) Quit\n");
+		printf("Choice: ");
+
+		scanf("%d", &choice);
+
+		if (choice == 0)
+		{
+			printf("Bye!\n");
+		}
+	}
+
+
+	
+	return (0);
+}
