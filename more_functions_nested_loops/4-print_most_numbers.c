15f13cb0764d59ace08bbf02c1b32ced25d2a067
Lujain Alsultan <lujainabdulmohsen@hotmail.com>
2026-07-09 20:44:04 +0300

    Fix print_most_numbers style

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -5,12 +5,13 @@
  */
 void print_most_numbers(void)
 {
- int n;
+	int n;
 
- for (n = 0; n <= 9; n++)
- {
-  if (n != 2 && n != 4)
-   _putchar(n + '0');
- }
- _putchar('\n');
+	for (n = 0; n <= 9; n++)
+	{
+		if (n != 2 && n != 4)
+			_putchar(n + '0');
+	}
+
+	_putchar('\n');
 }
