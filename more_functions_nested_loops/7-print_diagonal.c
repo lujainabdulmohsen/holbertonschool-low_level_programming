80d0f3261b859083967a3307b7487bde5a4ff35c
Lujain Alsultan <lujainabdulmohsen@hotmail.com>
2026-07-09 20:20:40 +0300

    Fix diagonal style

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -6,20 +6,20 @@
  */
 void print_diagonal(int n)
 {
- int i, j;
+	int i, j;
 
- if (n <= 0)
- {
-  _putchar('\n');
-  return;
- }
+	if (n <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
 
- for (i = 0; i < n; i++)
- {
-  for (j = 0; j < i; j++)
-   _putchar(' ');
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < i; j++)
+			_putchar(' ');
 
-  _putchar('\\');
-  _putchar('\n');
- }
+		_putchar('\\');
+		_putchar('\n');
+	}
 }
