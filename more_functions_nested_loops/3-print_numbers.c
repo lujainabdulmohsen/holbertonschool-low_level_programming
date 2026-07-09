cd104329ca25f33cc851acebdd867eaf7e9b01e4
Lujain Alsultan <lujainabdulmohsen@hotmail.com>
2026-07-09 20:41:23 +0300

    Fix print_numbers style

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -5,12 +5,12 @@
  */
 void print_numbers(void)
 {
- int n;
+	int n;
 
- for (n = 0; n <= 9; n++)
- {
-  _putchar(n + '0');
- }
+	for (n = 0; n <= 9; n++)
+	{
+		_putchar(n + '0');
+	}
 
- _putchar('\n');
+	_putchar('\n');
 }
