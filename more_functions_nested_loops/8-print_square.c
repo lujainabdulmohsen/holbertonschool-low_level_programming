8f83018b3650c65e104912409655e213d722afcb
Lujain Alsultan <lujainabdulmohsen@hotmail.com>
2026-07-09 20:17:32 +0300

    Fix square style

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -6,19 +6,19 @@
  */
 void print_square(int size)
 {
- int i, j;
+	int i, j;
 
- if (size <= 0)
- {
-  _putchar('\n');
-  return;
- }
+	if (size <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
 
- for (i = 0; i < size; i++)
- {
-  for (j = 0; j < size; j++)
-   _putchar('#');
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size; j++)
+			_putchar('#');
 
-  _putchar('\n');
- }
+		_putchar('\n');
+	}
 }
