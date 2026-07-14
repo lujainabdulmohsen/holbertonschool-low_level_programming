3ad9d5e1d1bc9267f2c11fba12142806e64d92d1
Lujain Alsultan <lujainabdulmohsen@hotmail.com>
2026-07-14 18:14:55 +0300

    Add leet function

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * leet - encodes a string into 1337
+ * @str: string
+ *
+ * Return: pointer to string
+ */
+char *leet(char *str)
+{
+	int i, j;
+	char l[] = "aAeEoOtTlL";
+	char n[] = "4433007711";
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (j = 0; l[j] != '\0'; j++)
+		{
+			if (str[i] == l[j])
+				str[i] = n[j];
+		}
+	}
+
+	return (str);
+}
