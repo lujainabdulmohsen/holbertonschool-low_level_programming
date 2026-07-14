7d100c44733c251f96af0396ab84a4a5ec0943f7
Lujain Alsultan <lujainabdulmohsen@hotmail.com>
2026-07-14 18:21:36 +0300

    Add magic array solution

--- pointers_arrays_strings/102-magic.c
+++ pointers_arrays_strings/102-magic.c
@@ -7,12 +7,11 @@
  */
 int main(void)
 {
-	int n;
+	int a[5];
 	int *p;
 
-	n = 402;
-	p = &n;
-	((char *)p)[5] = 98;
-	printf("a[2] = %d\n", n);
+	p = a;
+	*(p + 2) = 98;
+	printf("a[2] = %d\n", a[2]);
 	return (0);
 }
