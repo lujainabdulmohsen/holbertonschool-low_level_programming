f941fd78a2d47cff18df312cb28eb5e85094be96
Lujain Alsultan <lujainabdulmohsen@hotmail.com>
2026-07-26 13:23:16 +0300

    Add simple calculator

---
+++ function_pointers/3-main.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
+
+/**
+ * main - performs simple operations
+ * @argc: number of arguments
+ * @argv: argument vector
+ *
+ * Return: 0 on success, 98, 99 or 100 on error
+ */
+int main(int argc, char *argv[])
+{
+	int num1;
+	int num2;
+	int (*operation)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+	operation = get_op_func(argv[2]);
+
+	if (operation == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	if ((argv[2][0] == '/' || argv[2][0] == '%') && num2 == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	printf("%d\n", operation(num1, num2));
+	return (0);
+}
