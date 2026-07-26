d74ed77cde0ec269c82a8bf599dcce80334ac0a5
Lujain Alsultan <lujainabdulmohsen@hotmail.com>
2026-07-26 13:24:24 +0300

    Add main opcodes program

---
+++ function_pointers/100-main_opcodes.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints the opcodes of its own main function
+ * @argc: number of arguments
+ * @argv: argument vector
+ *
+ * Return: 0 on success, 1 or 2 on error
+ */
+int main(int argc, char *argv[])
+{
+	int bytes;
+	int i;
+	unsigned char *main_address;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	bytes = atoi(argv[1]);
+
+	if (bytes < 0)
+	{
+		printf("Error\n");
+		return (2);
+	}
+
+	main_address = (unsigned char *)main;
+
+	for (i = 0; i < bytes; i++)
+	{
+		printf("%02x", main_address[i]);
+
+		if (i < bytes - 1)
+			printf(" ");
+	}
+
+	printf("\n");
+	return (0);
+}
