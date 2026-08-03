a70b53d432ab8870f9e70c40c4f16ff67b9c9682
Lujain Alsultan <lujainabdulmohsen@hotmail.com>
2026-08-03 19:16:01 +0300

    Complete segmentation fault crash analysis

---
+++ ai_memory_visualizer_valgrind_tracer/programs/crashing_program.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+	int *number;
+
+	number = malloc(sizeof(int));
+	if (number == NULL)
+		return (1);
+
+	*number = 42;
+	free(number);
+	number = NULL;
+
+	printf("%d\n", *number);
+
+	return (0);
+}
