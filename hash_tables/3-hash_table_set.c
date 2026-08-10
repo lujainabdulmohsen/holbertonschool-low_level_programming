a80aaaa84c14e143a117f4c7eb238940d969a335
Lujain Alsultan <lujainabdulmohsen@hotmail.com>
2026-08-10 18:57:36 +0300

    Fix Betty style for hash table set

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -3,10 +3,40 @@
 #include <string.h>
 
 /**
+ * create_node - creates a new hash node
+ * @key: key
+ * @value: value
+ * @next: next node
+ *
+ * Return: pointer to new node, or NULL
+ */
+hash_node_t *create_node(const char *key, const char *value, hash_node_t *next)
+{
+	hash_node_t *node;
+
+	node = malloc(sizeof(hash_node_t));
+	if (node == NULL)
+		return (NULL);
+
+	node->key = strdup(key);
+	node->value = strdup(value);
+	if (node->key == NULL || node->value == NULL)
+	{
+		free(node->key);
+		free(node->value);
+		free(node);
+		return (NULL);
+	}
+
+	node->next = next;
+	return (node);
+}
+
+/**
  * hash_table_set - adds an element to the hash table
  * @ht: hash table
  * @key: key
- * @value: value associated with the key
+ * @value: value
  *
  * Return: 1 on success, 0 on failure
  */
@@ -20,9 +50,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		return (0);
 
 	index = key_index((const unsigned char *)key, ht->size);
-	node = ht->array[index];
-
-	while (node != NULL)
+	for (node = ht->array[index]; node != NULL; node = node->next)
 	{
 		if (strcmp(node->key, key) == 0)
 		{
@@ -33,30 +61,12 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 			node->value = new_value;
 			return (1);
 		}
-		node = node->next;
 	}
 
-	node = malloc(sizeof(hash_node_t));
+	node = create_node(key, value, ht->array[index]);
 	if (node == NULL)
 		return (0);
 
-	node->key = strdup(key);
-	if (node->key == NULL)
-	{
-		free(node);
-		return (0);
-	}
-
-	node->value = strdup(value);
-	if (node->value == NULL)
-	{
-		free(node->key);
-		free(node);
-		return (0);
-	}
-
-	node->next = ht->array[index];
 	ht->array[index] = node;
-
 	return (1);
 }
